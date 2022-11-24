906f3c196e24c48fbe827ac3591fd68fe1f416ce
Jack Jin <jackjin.com@gmail.com>
2022-11-23 21:23:03 -0500

    move in project.

---
+++ Practical.AspNetCore.SignalR/Hubs/MessageHub.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace Practical.AspNetCore.SignalR.Hubs
+{
+    public class MessageHub: Hub
+    {
+        public async Task SendMessageToAll(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendMessageToCaller(string user, string message)
+        {
+            await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendMessageToUser(string connectionId, string user, string message)
+        {
+            await Clients.Client(connectionId).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public Task JoinGroup(string group)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public async Task SendMessageToGroup(string group, string user, string message)
+        {
+            await Clients.Group(group).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ Practical.AspNetCore.SignalR/wwwroot/js/message.js
@@ -0,0 +1,77 @@
+﻿"use strict"
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/messages").build();
+
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var div = document.createElement("div");
+    div.innerHTML = `${user} say ${msg}` + "<hr />";
+    document.getElementById("messages").appendChild(div);
+});
+
+connection.on("UserConnected", function (connectionId) {
+    var groupElement = document.getElementById("group");
+    var option = document.createElement("option");
+    option.text = connectionId;
+    option.value = connectionId;
+    groupElement.add(option);
+});
+
+connection.on("UserDisconnected", function (connectionId) {
+    var groupElement = document.getElementById("group");
+    for (var i = 0; i < groupElement.length; i++) {
+        if (groupElement.options[i].value == connectionId) {
+            groupElement.remove(i);
+        }
+    }
+})
+
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("user").value;
+    var message = document.getElementById("message").value;
+    var groupElement = document.getElementById("group");
+    var groupValue = groupElement.options[groupElement.selectedIndex].value;
+
+    if (groupValue === "All" || groupValue === "Myself") {
+        var method = "SendMessageToAll";
+        if (groupValue === "Myself") {
+            method = "SendMessageToCaller";
+        }
+
+        connection.invoke(method, user, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+    else if (groupValue === "PrivateGroup") {
+        var method = "SendMessageToGroup"
+        connection.invoke(method, "PrivateGroup", user, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+    else {
+        var method = "SendMessageToUser"
+        connection.invoke(method, groupValue, user, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+
+    event.preventDefault();
+});
+
+document.getElementById("joinGroup").addEventListener("click", function () {
+    connection.invoke("JoinGroup", "PrivateGroup").catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    event.preventDefault();
+});
+
